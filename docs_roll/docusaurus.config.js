e2b1fafeefca4edaf48dff586f64b3fe622d4249
kkkky123 <neveryan211@163.com>
2025-08-11 19:28:48 +0800

    feat: add home page to docs

--- docs_roll/docusaurus.config.js
+++ docs_roll/docusaurus.config.js
@@ -42,7 +42,8 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/alibaba/ROLL/tree/main/docs_roll/',
+          showLastUpdateTime: true
         },
         blog: {
           showReadingTime: true,
--- docs_roll/src/pages/index.js
+++ docs_roll/src/pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Button } from 'antd';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import {useHistory} from '@docusaurus/router';
 
 import styles from './index.module.css';
 
@@ -12,10 +14,20 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <img src="img/roll.jpeg" alt="ROLL" style={{ maxWidth: '50%' }} />
+        </div>
+        <div className={styles.content}>
+          {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+          {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+          <div className={styles.left}>
+            <div className={styles.desc}>
+              ROLL is an efficient and user-friendly RL library designed for Large Language Models (LLMs) utilizing Large Scale GPU resources. It significantly enhances LLM performance in key areas such as human preference alignment, complex reasoning, and multi-turn agentic interaction scenarios.
+            </div>
+            <div>
+              <Button style={{ height: '60px', fontSize: '20px', padding: '0 20px'}} size="large" type="primary" href="/ROLL/docs/简体中文/快速开始/multi_nodes_quick_start_cn">Get started</Button>
+            </div>
+          </div>
         </div>
       </div>
     </header>
@@ -24,6 +36,12 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+
+  const history = useHistory();
+  useEffect(() => {
+    history.replace('/ROLL/docs/English/QuickStart/start');
+  }, [history]);
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
